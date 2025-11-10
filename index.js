6e7a0335fda2c4a0f30fc6101f7a592aa6b4fe62
Leonardo Sardella <tu_email@ejemplo.com>
2025-11-10 14:05:14 -0300

    Backend listo para Render

--- index.js
+++ index.js
@@ -22,4 +22,10 @@ app.use('/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
+
+app.get('/', (req, res) => {
+  res.send('API Padel Backend funcionando 🚀');
+
+
+
 });
