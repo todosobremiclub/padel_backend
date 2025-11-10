b7dfe042cd7e42194505dffa09359cca6b3e741a
Leonardo Sardella <tu_email@ejemplo.com>
2025-11-10 17:13:39 -0300

    Fix: corregido error en rutas de clubes.js

--- routes/clubes.js
+++ routes/clubes.js
@@ -11,10 +11,10 @@ const {
 const verifyToken = require('../middlewares/authMiddleware');
 const allowRoles = require('../middlewares/roleMiddleware');
 
-// Endpoints protegidos
 router.get('/', verifyToken, allowRoles('SUPER_ADMIN', 'CLUB_ADMIN'), getClubes);
 router.get('/:id', verifyToken, allowRoles('SUPER_ADMIN', 'CLUB_ADMIN'), getClubById);
 router.post('/', verifyToken, allowRoles('SUPER_ADMIN'), createClub);
 router.put('/:id', verifyToken, allowRoles('SUPER_ADMIN'), updateClub);
 router.delete('/:id', verifyToken, allowRoles('SUPER_ADMIN'), deleteClub);
 
+module.exports = router;
