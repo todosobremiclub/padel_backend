4a3e038501e5f9e9b59dde588b80636d5e82d9cf
Leonardo Sardella <tu_email@ejemplo.com>
2025-11-12 17:53:12 -0300

    Actualización en rutas de clubes

--- routes/clubes.js
+++ routes/clubes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const verifyToken = require('../middlewares/authMiddleware');
+const allowRoles = require('../middlewares/roleMiddleware');
 const {
   getClubes,
   getClubById,
@@ -9,9 +11,7 @@ const {
   deleteClub
 } = require('../controllers/clubesController');
 
-const verifyToken = require('../middlewares/authMiddleware');
-const allowRoles = require('../middlewares/roleMiddleware');
-
+// Endpoints protegidos
 router.get('/', verifyToken, allowRoles('SUPER_ADMIN', 'CLUB_ADMIN'), getClubes);
 router.get('/:id', verifyToken, allowRoles('SUPER_ADMIN', 'CLUB_ADMIN'), getClubById);
 router.post('/', verifyToken, allowRoles('SUPER_ADMIN'), createClub);
