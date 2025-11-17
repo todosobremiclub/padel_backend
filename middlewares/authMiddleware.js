1b9c7f481bec4d32565dbbe7146018b9422ed191
Leonardo Sardella <tu_email@ejemplo.com>
2025-11-17 13:44:19 -0300

    Agrego console.log para verificar contenido del token y rol en autenticación

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,14 @@
 const jwt = require('jsonwebtoken');
-
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-
   if (!token) return res.status(403).json({ message: 'Token requerido' });
-
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: 'Token inválido' });
     req.user = user;
+    // Imprime el contenido del token en la consola
+    console.log('Usuario autenticado:', req.user);
     next();
   });
 };
-
-module.exports = verifyToken; // ✅ IMPORTANTE
+module.exports = verifyToken;
